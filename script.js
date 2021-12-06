92c09ff8cf4326823f5de9c2ed336fcc1d1ec0cf
Hari sudhan <harisudhan1008@gmail.com>
2021-12-06 11:55:16 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+let span = document.getElementById("span")
+span.innerHTML = 10
+
+let count = 10;
+setTimeout(()=>{
+    count = count-1
+    span.innerHTML = count
+    setTimeout(()=>{
+        count = count-1
+        span.innerHTML = count
+        setTimeout(()=>{
+            count = count-1
+            span.innerHTML = count
+            setTimeout(()=>{
+                count = count-1
+                span.innerHTML = count
+                setTimeout(()=>{
+                    count = count-1
+                    span.innerHTML = count
+                    setTimeout(()=>{
+                        count = count-1
+                        span.innerHTML = count
+                        setTimeout(()=>{
+                            count = count-1
+                            span.innerHTML = count
+                            setTimeout(()=>{
+                                count = count-1
+                                span.innerHTML = count
+                                setTimeout(()=>{
+                                    count = count-1
+                                    span.innerHTML = count
+                                    setTimeout(()=>{
+                                        count = count-1
+                                        span.innerHTML = "Happy Independence Day"
+                                        
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
